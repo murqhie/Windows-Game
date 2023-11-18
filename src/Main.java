f7d76794f43f5f1b38c94b7751fd6c68fa3a05e6
Murphy <isc.merve@gmail.com>
2023-11-18 19:36:26 +0100

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import processing.core.PApplet;
+
+/**
+ * Just a basic Processing template to check if your IDE is configured correctly.
+ */
+public class Main extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main(Main.class);
+    }
+
+    public Main() {}
+
+    @Override
+    public void settings() {
+        setSize(600, 400);
+    }
+
+    @Override
+    public void setup() {
+    }
+
+    @Override
+    public void draw() {
+        background(255);
+    }
+}
