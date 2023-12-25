7450f76b6955ecec8cfa09b6761ca8a4336906e1
Murphy <isc.merve@gmail.com>
2023-12-25 13:54:18 +0100

    kack thread scheiß kack ding blody

---
+++ src/Views/SpriteLoader.java
@@ -0,0 +1,45 @@
+package Views;
+import processing.core.PImage;
+import processing.core.PApplet;
+
+import java.io.File;
+import java.util.HashMap;
+
+
+public class SpriteLoader extends PApplet implements Runnable {
+    private HashMap<String, PImage> sprites = new HashMap<>();
+    private File dir;
+    int screenHeight;
+    int screenWidth;
+    int scaleFactor;
+
+    public SpriteLoader(int screenWidth,int screenHeight) {
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+        this.scaleFactor = screenHeight/360;
+        this.dir = new File("images");
+    }
+
+    @Override
+    public void run() {
+
+        for (File file : dir.listFiles()) {
+            loadSprite(file);
+        }
+    }
+
+    public PImage getSprite(String name) {
+        return sprites.get(name);
+    }
+
+    private void loadSprite(File file) {
+        String name = file.getName().split("\\.")[0];
+        int pixelWidth = parseInt(file.getName().split("\\.")[1]);
+        int pixelHeight = parseInt(file.getName().split("\\.")[2]);;
+        PImage image = loadImage(sketchPath() + "/images/" + file.getName());
+
+        image.resize(pixelWidth *scaleFactor, pixelHeight *scaleFactor);
+        sprites.put(name, image);
+        System.out.println("df");
+    }
+}
--- src/Views/View.java
+++ src/Views/View.java
@@ -7,7 +7,6 @@ import Models.Objects.Player;
 import Models.Objects.Projectile;
 import Models.Objects.Window;
 import processing.core.PApplet;
-import processing.core.PImage;
 import processing.event.KeyEvent;
 import processing.event.MouseEvent;
 
@@ -15,27 +14,33 @@ import java.util.ArrayList;
 
 public class View extends PApplet implements IView{
     IController controller;
-    PImage desktopImage;
-    public View(){  }
+    SpriteLoader sprites;
+
     @Override
     public void settings() {
         size(1920,1080);
-
       //fullScreen();
     }
     @Override
     public void setup() {
-        controller.setGameState("START");
-        desktopImage = loadImage("Desktop.png");
-        desktopImage.resize(width,height);
 
+        sprites = new SpriteLoader(width,height);
+        Thread thread = new Thread(sprites);
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        controller.setGameState("START");
     }
     public void draw(){
         controller.tick();
     }
     public void drawPlaying(){
 
-        background(desktopImage);
+        background(sprites.getSprite("Desktop"));
         drawWindow(controller.getMainWindow());
         for (Window window : controller.getEnemyWindows()) {
             drawWindow(window);
@@ -59,13 +64,20 @@ public class View extends PApplet implements IView{
         textAlign(LEFT,TOP);
         textSize(150);
         fill(255);
-        text(":(", 100, (float) (height * 0.3));
+        text(":(", 200, (float) (height * 0.2));
         textSize(50);
-        text(String.format("Game Over! Press Enter to restart.\nYour score was %d and the highscore was %d.", controller.getScore()/10, controller.getHighScore()/10), 100, (float) (height * 0.45));
+        text(String.format("""
+                Your PC ran into a Problem\s
+                ERROR: Game Over.
+
+                Press [SPACE] to initiate system reboot.
+                Your score was %d and the highscore was %d.""", controller.getScore()/10, controller.getHighScore()/10), 200, (float) (height * 0.35));
+        image(sprites.getSprite("QR"), 200, (float) (height * 0.65));
+
 
     }
     public void drawStart(){
-        background(255);
+        background(sprites.getSprite("Desktop"));
         textAlign(CENTER,CENTER);
         textSize(100);
         fill(150,50,50);
